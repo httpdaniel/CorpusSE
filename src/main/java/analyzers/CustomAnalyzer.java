3c5dceabe5875300beebd706b5437e1473d50afc
Matt Murtagh <m.murtagh-white.fs@odi.org.uk>
2020-12-14 10:57:25 +0000

    n grams

--- src/main/java/analyzers/CustomAnalyzer.java
+++ src/main/java/analyzers/CustomAnalyzer.java
@@ -1,8 +1,11 @@
 package analyzers;
 import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.en.EnglishMinimalStemFilter;
+import org.apache.lucene.analysis.en.EnglishPossessiveFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
 import org.apache.lucene.analysis.ngram.NGramTokenFilter;
 
 // https://www.baeldung.com/lucene-analyzers
@@ -10,11 +13,19 @@ import org.apache.lucene.analysis.ngram.NGramTokenFilter;
 public class CustomAnalyzer extends StopwordAnalyzerBase {
 
 	private int ngrams;
-
-	public CustomAnalyzer(CharArraySet stopwords, int _ngrams) {
+	
+	private final CharArraySet stemExclusionSet;
+	
+	  public CustomAnalyzer(CharArraySet stopwords, int _ngrams) {
+		    this(stopwords, _ngrams, CharArraySet.EMPTY_SET);
+		  }
+	  
+	public CustomAnalyzer(CharArraySet stopwords, int _ngrams,CharArraySet stemExclusionSet) {
         super(stopwords);
+	    this.stemExclusionSet = CharArraySet.unmodifiableSet(CharArraySet.copy(stemExclusionSet));
         ngrams = _ngrams;
     }
+	
 
 
     public CharArraySet getStopwords() {
@@ -25,18 +36,27 @@ public class CustomAnalyzer extends StopwordAnalyzerBase {
         return ngrams;
     }
 
-    @Override
+    @SuppressWarnings("resource")
+	@Override
     protected TokenStreamComponents createComponents(String fieldName) {
         final StandardTokenizer tokenizer = new StandardTokenizer();
 
-        // Create token stream and add filters
-        TokenStream filter = new LowerCaseFilter(tokenizer);
 
-        filter = new PorterStemFilter(filter);
-        filter = new StopFilter(filter, stopwords);
-        filter = new EnglishMinimalStemFilter(filter);
- 	    filter = new NGramTokenFilter(filter, ngrams);
-
-        return new TokenStreamComponents(tokenizer, filter);
+       // filter = new PorterStemFilter(filter);
+       // filter = new StopFilter(filter, stopwords);
+        //filter = new EnglishMinimalStemFilter(filter);
+ 	    //filter = new NGramTokenFilter(filter, ngrams);
+ 	    
+ 	    final Tokenizer source = new StandardTokenizer();
+ 	    TokenStream filter = new EnglishPossessiveFilter(source);
+ 	   filter = new LowerCaseFilter(filter);
+ 	   filter = new StopFilter(filter, stopwords);
+ 	    if(!stemExclusionSet.isEmpty())
+ 	    	filter = new SetKeywordMarkerFilter(filter, stemExclusionSet);
+ 	   filter = new PorterStemFilter(filter);
+ 	    
+ 	  
+ 	   
+        return new TokenStreamComponents(source, filter);
     }
 }
